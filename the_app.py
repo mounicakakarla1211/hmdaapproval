d640719387aac18f4c9c0db89c13e7f1726452b1
mounicakakarla1211 <56417857+mounicakakarla1211@users.noreply.github.com>
2023-05-16 02:12:34 -0400

    Update the_app.py

--- the_app.py
+++ the_app.py
@@ -108,8 +108,7 @@ if st.button('Submit'):
     catVal = np.array([applicant_age, applicant_sex, co_applicant_sex, applicant_ethnicity_1, co_applicant_ethnicity_1, applicant_race_1, co_applicant_race_1, state_code,
               debt_to_income_ratio, applicant_credit_scoring_model, co_applicant_credit_scoring_model, loan_type, loan_purpose, construction_method, occupancy_type,
               purchaser_type, business_or_commercial_purpose])
-    catVal_t = catVal.reshape(1,-1)
-    df_cat = pd.DataFrame(catVal_t, index=[0], columns=["applicant_age", "applicant_sex", "co_applicant_sex", "applicant_ethnicity_1", "co_applicant_ethnicity_1", "applicant_race_1", "co_applicant_race_1", "state_code",
+    df_cat = pd.DataFrame(catVal, index=[0], columns=["applicant_age", "applicant_sex", "co_applicant_sex", "applicant_ethnicity_1", "co_applicant_ethnicity_1", "applicant_race_1", "co_applicant_race_1", "state_code",
               "debt_to_income_ratio", "applicant_credit_scoring_model", "co_applicant_credit_scoring_model", "loan_type", "loan_purpose", "construction_method", "occupancy_type",
               "purchaser_type", "business_or_commercial_purpose"])
     catDF = pd.get_dummies(df_cat, drop_first=True)
