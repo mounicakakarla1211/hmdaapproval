db4efffcd1057046e65f65241a8a7ea883477189
mounicakakarla1211 <56417857+mounicakakarla1211@users.noreply.github.com>
2023-05-15 21:54:14 -0400

    Update the_app.py

--- the_app.py
+++ the_app.py
@@ -3,6 +3,8 @@ import streamlit as st
 import joblib
 import numpy as np
 from sklearn import datasets
+def format_func(option):
+    return CHOICES[option]
 
 st.title("HMDA Loan Approval Predictor")
 
@@ -14,7 +16,8 @@ loan_amount = st.text_input('Loan Amount', '')
 income = st.text_input('Income', '')
 loan_term = st.text_input('Loan Term', '')
 combined_loan_to_value_ratio = st.text_input('Combined Loan To Value Ratio', '')
-loan_type = st.selectbox('Loan Type',(1, 2, 3))
+loan_type_choices = {1: "1", 2: "2", 3: "3"}
+loan_type = st.selectbox('Loan Type',options=list(loan_type_choices.keys()), format_func=format_func))
 sepal_width = st.slider("Sepal Width (cm)", 2.0, 4.4, 3.1)
 petal_length = st.slider("Petal Length (cm)", 1.0, 6.9, 4.7)
 petal_width = st.slider("Petal Width (cm)", 0.1, 2.5, 1.4)
