32eddf4e1482d3238192297fe71cc4894e6625e4
mounicakakarla1211 <56417857+mounicakakarla1211@users.noreply.github.com>
2023-05-15 22:00:41 -0400

    Update the_app.py

--- the_app.py
+++ the_app.py
@@ -17,11 +17,9 @@ loan_amount = st.text_input('Loan Amount', '')
 income = st.text_input('Income', '')
 loan_term = st.text_input('Loan Term', '')
 combined_loan_to_value_ratio = st.text_input('Combined Loan To Value Ratio', '')
-loan_type_choices = {1: "1", 2: "2", 3: "3"}
+loan_type_choices = {1: "10", 2: "22", 3: "32"}
 loan_type = st.selectbox('Loan Type',options=list(loan_type_choices.keys()), format_func=format_func)
-sepal_width = st.slider("Sepal Width (cm)", 2.0, 4.4, 3.1)
-petal_length = st.slider("Petal Length (cm)", 1.0, 6.9, 4.7)
-petal_width = st.slider("Petal Width (cm)", 0.1, 2.5, 1.4)
+
 
 #features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
 
