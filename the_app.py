c81840ec0b3e221939dcf7a40fd1220930a9bea9
mounicakakarla1211 <56417857+mounicakakarla1211@users.noreply.github.com>
2023-05-15 22:04:56 -0400

    Update the_app.py

--- the_app.py
+++ the_app.py
@@ -12,13 +12,15 @@ st.title("HMDA Loan Approval Predictor")
 st.write("""
 Enter the application details:
 """)
-
-loan_amount = st.text_input('Loan Amount', '')
-income = st.text_input('Income', '')
-loan_term = st.text_input('Loan Term', '')
-combined_loan_to_value_ratio = st.text_input('Combined Loan To Value Ratio', '')
-loan_type_choices = {1: "1", 2: "2", 3: "3"}
-loan_type = st.selectbox('Loan Type',options=list(loan_type_choices.keys()), format_func=format_func)
+col1, col2, col3 = st.columns(3)
+with col1:
+    loan_amount = st.text_input('Loan Amount', '')
+    income = st.text_input('Income', '')
+    loan_term = st.text_input('Loan Term', '')
+    combined_loan_to_value_ratio = st.text_input('Combined Loan To Value Ratio', '')
+with col2:
+    loan_type_choices = {1: "1", 2: "2", 3: "3"}
+    loan_type = st.selectbox('Loan Type',options=list(loan_type_choices.keys()), format_func=format_func)
 
 
 #features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
