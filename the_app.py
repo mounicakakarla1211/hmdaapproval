673e9d772560bb185e81990e4fc00b85de101466
mounicakakarla1211 <56417857+mounicakakarla1211@users.noreply.github.com>
2023-05-16 00:24:52 -0400

    Update the_app.py

--- the_app.py
+++ the_app.py
@@ -2,7 +2,9 @@
 import streamlit as st
 import joblib
 import numpy as np
+import pandas as pd
 from sklearn import datasets
+from sklearn.preprocessing import RobustScaler
 
 def applicant_age_func(option):
     return applicant_age_choices[option]
@@ -90,10 +92,23 @@ with col3:
     purchaser_type = st.selectbox('Purchase Type',options=list(purchaser_type_choices.keys()), format_func=purchaser_type_func)
     business_or_commercial_purpose_choices = {"1": "1", "2": "2"}
     business_or_commercial_purpose = st.selectbox('Business or Commericial Purpose',options=list(business_or_commercial_purpose_choices.keys()), format_func=business_or_commercial_purpose_func)
-#features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
 
-#iris_rf_model = joblib.load('iris_rf_model.pkl')
-#prediction = iris_rf_model.predict(features)
+income_log = np.log(income)
+loan_amount_log = np.log(loan_amount)
+numVal = [combined_loan_to_value_ratio,loan_term,income_log,loan_amount_log]
+df_num = pd.DataFrame(numVal, columns=["combined_loan_to_value_ratio","loan_term","income_log","loan_amount_log"])
+catVal = [applicant_age,applicant_sex,co_applicant_sex,applicant_ethnicity_1,co_applicant_ethnicity_1,applicant_race_1,co_applicant_race_1,state_code,
+          debt_to_income_ratio,applicant_credit_scoring_model,co_applicant_credit_scoring_model,loan_type,loan_purpose,construction_method,occupancy_type,
+          purchaser_type,business_or_commercial_purpose]
+df_cat =pd.DataFrame(catVal, columns=["applicant_age","applicant_sex","co_applicant_sex","applicant_ethnicity_1","co_applicant_ethnicity_1","applicant_race_1","co_applicant_race_1","state_code",
+          "debt_to_income_ratio","applicant_credit_scoring_model","co_applicant_credit_scoring_model","loan_type","loan_purpose","construction_method","occupancy_type",
+          "purchaser_type","business_or_commercial_purpose"])
+scaler = RobustScaler()
+numDF = pd.DataFrame(scaler.fit_transform(df_num.values),columns=df_num.columns,index=df_num.index)
+catDF = pd.get_dummies(df_cat,drop_first=True)
+X = pd.concat([catDF, numDF],axis=1)
+hmdamodel = joblib.load('iris_rf_model.pkl')
+prediction = hmdamodel.predict(X)
 
 st.subheader("Prediction:")
-st.write(loan_type)
+st.write(prediction)
