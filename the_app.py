95ce8c40c8ef56c65b7fc1c747e80c0c4c5c6e5d
mounicakakarla1211 <56417857+mounicakakarla1211@users.noreply.github.com>
2023-05-16 00:43:05 -0400

    Update the_app.py

--- the_app.py
+++ the_app.py
@@ -96,7 +96,9 @@ with col3:
 if st.button('Submit'):
     income_log = math.log(int(income))
     loan_amount_log = math.log(float(loan_amount))
-    numVal = [float(combined_loan_to_value_ratio),float(loan_term),income_log,loan_amount_log]
+    clvr = float(combined_loan_to_value_ratio)
+    lt = float(loan_term)
+    numVal = [clvr,lt,income_log,loan_amount_log]
     df_num = pd.DataFrame(numVal, columns=["combined_loan_to_value_ratio","loan_term","income_log","loan_amount_log"])
     catVal = [applicant_age,applicant_sex,co_applicant_sex,applicant_ethnicity_1,co_applicant_ethnicity_1,applicant_race_1,co_applicant_race_1,state_code,
               debt_to_income_ratio,applicant_credit_scoring_model,co_applicant_credit_scoring_model,loan_type,loan_purpose,construction_method,occupancy_type,
