649bf310254e61dd23f0869034042f53459d6f2a
mounicakakarla1211 <56417857+mounicakakarla1211@users.noreply.github.com>
2023-05-15 21:44:51 -0400

    Update the_app.py

--- the_app.py
+++ the_app.py
@@ -11,6 +11,9 @@ Enter the application details:
 """)
 
 loan_amount = st.text_input('Loan Amount', '')
+income = st.text_input('Income', '')
+loan_term = st.text_input('Loan Term', '')
+combined_loan_to_value_ratio = st.text_input('Combined Loan To Value Ratio', '')
 sepal_width = st.slider("Sepal Width (cm)", 2.0, 4.4, 3.1)
 petal_length = st.slider("Petal Length (cm)", 1.0, 6.9, 4.7)
 petal_width = st.slider("Petal Width (cm)", 0.1, 2.5, 1.4)
